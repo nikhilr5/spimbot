d6491ad4fb6c0a6d220c306be56d74826caf4479
Nikhil Richard <42984048+nikhilr5@users.noreply.github.com>
2020-05-03 21:24:06 -0500

    Add files via upload

--- spimbot.s
+++ spimbot.s
@@ -118,12 +118,12 @@ other_infinite_loop:
 
 for:
 
-    bgt		$t5, 5000, pastfor	# if $t5 > 5000 then pastfor
+    bgt		$t5, 4500, pastfor	# if $t5 > 5000 then pastfor
     add $t5, $t5, 1
     j for
 
 pastfor:
-    li $t0, 10
+    li $t0, 30
 
     bgt $s1, $t0, pastAngleControl
 
@@ -135,7 +135,7 @@ pastfor:
     lbu $t1, 2($t0) # get tile_type
 
 
-    li $t2, 4 # t2= 4 friendly
+    li $t2, 4 # t2= 4 friendlys
     li $t3, 2 #t3 = 2 #host_mask
     li $t4, 16 #t4 = 16 player_mask
     li $t5, 8 #t5 = 8 enemy_mask
@@ -145,12 +145,33 @@ pastfor:
     and $t4, $t4, $t1 #scanner_data->tile_type & PLAYER_MASK
     and $t5, $t5, $t1 #scanner_data->tile_type & enemy
 
-    ble		$t4, $0, pastif	# if Player_mask > 0 then load_number
-    ble		$t5, $0, pastif	# if $t5 <= $0 then target
+    ble		$t4, $0, next	# if Player_mask < 0 then load_number
+    ble		$t5, $0, next	# if $t5 < $0 then target
     
+    
+    li $t2, 0
+    li $t4, 1
+
+shoot_for_loop:
+    li $t3, 5
+    bge $t2 $t3, pastAngleControl
 
     sw $0, SHOOT_UDP_PACKET #shoot udp
-    j pastAngleControl
+
+    li $t0, 0
+    sw $t0, ANGLE_CONTROL
+
+    li $t1, 3 #shift my degree 3
+        
+
+
+    sw $t1, ANGLE
+    sw $t0, VELOCITY
+
+
+     add $t2, $t2, 1
+
+    j shoot_for_loop
 
 next:
     bgt		$t2, $0, pastif	# if friendly_mask detected >= 0 then pastif
@@ -159,46 +180,12 @@ next:
     j pastAngleControl
 
 
-load_number:
-    li $t2, 0
-    li $t4, 1
-    li $t1, 20
-shoot_for_loop:
-    li $t3, 20
-    bgt $t2, $t3, pastAngleControl
-    sw $0, SHOOT_UDP_PACKET #shoot udp
-    li $s1, 10 #set s0 equal to 10
-
-    li $t0, 0
-    sw $t0, ANGLE_CONTROL
-    #shoot like a fan
-
-    mul $t5, $t1, $t4 #get angle
-
-    sw $t5, ANGLE
-   # sw $t0, VELOCITY
-
-    bgt $t5, $0, positive
-    sub $t4, $t4, 1 #t4 - 1
-    mul $t4, $t4, -1 # t4 * -1
-    j past_new_angle
-
-postive: 
-    add $t4, $t4, 1
-    mul $t4, $t4, -1 # t4 * -1
-    j past_new_angle
-
-past_new_angle:
-    add $t2, $t2, 1
-
-    j shoot_for_loop
-
 
 pastif:
 
     li $t0, 0
     sw $t0, ANGLE_CONTROL
-    li $t1, 3 #shift by 3 degrees
+    li $t1, 12 #shift by 3 degrees
     sw $t1, ANGLE
     sw $t0, VELOCITY
     add $s1, $s1, 1 #s1++
